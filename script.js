0a3b90ac3aff85c82cf560a9c08cec9364d56076
bitcoin-loan <loanmanagementbitcoin@gmail.com>
2026-02-22 23:58:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ async function fetchBTCPrice() {
         let response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
         let data = await response.json();
         btcPrice = data.bitcoin.usd;
-        document.getElementById("btcPrice").innerText = "BTC Price: $" + btcPrice;
+
+        let priceElement = document.getElementById("btcPrice");
+        if(priceElement){
+            priceElement.innerText = "BTC Price: $" + btcPrice;
+        }
+
     } catch (error) {
-        document.getElementById("btcPrice").innerText = "BTC Price: $40,000";
         btcPrice = 40000;
+        let priceElement = document.getElementById("btcPrice");
+        if(priceElement){
+            priceElement.innerText = "BTC Price: $40,000";
+        }
     }
 }
 
